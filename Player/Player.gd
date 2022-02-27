46083c74fad8b52997e22f4276911c6b56c9e3b7
FranciscoSaraiva <francisco_saraiva94@hotmail.com>
2022-02-27 13:42:39 +0000

    basic animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,15 +6,26 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
+	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		if input_vector.x < 0:
+			animationPlayer.play("RunLeft")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		if input_vector.x > 0:
+			animationPlayer.play("IdleRight")
+		if input_vector.x < 0:
+			animationPlayer.play("IdleLeft")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
